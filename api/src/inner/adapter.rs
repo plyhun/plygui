3a3b094ffa6f498b55958055ca25f8e8404bfd25
Serhii Plyhun <snuk188@gmail.com>
2020-08-07 17:16:12 +0200

    for_each in adapter

--- api/src/inner/adapter.rs
+++ api/src/inner/adapter.rs
@@ -6,16 +6,17 @@ pub trait AdapterInner: 'static {
     fn on_item_change(&mut self, cb: Option<AdapterInnerCallback>);
 }
 
-pub trait AdapterIterator<'a>: Iterator<Item=(&'a [usize], Node)> {}
-
 pub trait Adapter: AdapterInner + AsAny {
 	fn len_at(&self, indexes: &[usize]) -> Option<usize>;
 	fn node_at(&self, indexes: &[usize]) -> Option<Node>;
 	fn spawn_item_view(&mut self, indexes: &[usize], parent: &dyn Adapted) -> Option<Box<dyn Control>>;
 	
-	/// Do not use collect methods of this iterator, because of the reference to the indexes - they will produce UB if collected.
-	unsafe fn into_items_indexes_iter<'a>(&'a self) -> Box<dyn AdapterIterator<'a> + 'a>;
+	fn for_each<'a, 'b:'a, 'c: 'b>(&'c self, f: &'a mut dyn FnNodeItem);
 }
+
+pub trait FnNodeItem: for<'i, 'v> FnMut(&'i [usize], &'v Node) {}
+
+impl<F: for<'i, 'v> FnMut(&'i [usize], &'v Node)> FnNodeItem for F {}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Node {
